d27439af96bf61f2f303b6c8bb2973b840f13521
sidereus <francesco.serafin.3@gmail.com>
2019-04-19 10:04:39 -0600

    init: dependencies

---
+++ install.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+
+install.packages(c("maptools","gtools","foreach",
+                   "doParallel","raster","rgdal",
+                   "sp","dplyr","Rserve","Rcpp"),
+repos="http://lib.stat.cmu.edu/R/CRAN")
